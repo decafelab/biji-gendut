c067a6de0839457ccb2d562cc913917fdfb23488
decafelab <decafelab@gmail.com>
2022-12-10 22:03:13 +0700

    navbar click

---
+++ data/header.js
@@ -0,0 +1,11 @@
+export const login = [
+  { href: "/login", label: "Member Login" },
+  { href: "", label: "Office Login" }
+];
+
+export const navigation = [
+  { title: "Works", href: "/works" },
+  { title: "About Us", href: "/about-us" },
+  { title: "Clients", href: "/clients" },
+  { title: "Contact", href: "/contact" }
+];
